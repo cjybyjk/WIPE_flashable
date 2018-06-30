f485069666524cdb5488cb5296ed98f4c6e8e442
sford <cjybyjk@gmail.com>
2018-06-30 17:07:33 +0800

    fix bug:magisk: all '/sdcard' are replaced with '/data/media/0' because '/sdcard' is not available during boot.

--- magisk_install/common/service.sh
+++ magisk_install/common/service.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 # Project WIPE support
-powercfg `cat /sdcard/wipe_mode`
+powercfg `cat /data/media/0/wipe_mode`
 if [[ -f "/system/etc/init.qcom.post_boot.sh" ]] || \
 	[[ -f "/vendor/bin/init.qcom.post_boot.sh" ]] ; then
 	rm -f /dev/.project_wipe
