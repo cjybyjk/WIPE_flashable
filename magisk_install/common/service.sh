c826ba152ab4d3b88fd93db09953b08c905248e5
sford <cjybyjk@gmail.com>
2018-07-04 00:42:33 +0800

    fix bug: read mode from internal storage

--- magisk_install/common/service.sh
+++ magisk_install/common/service.sh
@@ -1,6 +1,10 @@
 #!/system/bin/sh
 # Project WIPE support
-powercfg `cat /data/media/0/wipe_mode`
+if [ -f '/sdcard/wipe_mode' ]; then
+    powercfg `cat /sdcard/wipe_mode`
+else
+    powercfg `cat /data/media/0/wipe_mode`
+fi
 if [[ -f "/system/etc/init.qcom.post_boot.sh" ]] || \
 	[[ -f "/vendor/bin/init.qcom.post_boot.sh" ]] ; then
 	rm -f /dev/.project_wipe
