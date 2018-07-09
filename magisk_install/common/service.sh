c68d20df469a0dc6adb7efec29a09be737b8c7c7
sford <cjybyjk@gmail.com>
2018-07-10 00:04:44 +0800

    updater: return exitcode updater: read list to replace hard-coded files updater: try to enable magisk on treble device updater: move powercfg to /system/bin updater: replace powercfg with powercfg_real powercfg: replace powercfg with powercfg_real powercfg: enable backup&restore powercfg: output to file runWIPE: move SELinux config to powercfg service.sh: move SELinux config to powercfg

--- magisk_install/common/service.sh
+++ magisk_install/common/service.sh
@@ -1,6 +1,5 @@
 #!/system/bin/sh
 # Project WIPE support
-SEstatus=`getenforce`
 MODE=`cat /sdcard/wipe_mode`
 if [ "" = "$MODE" ]; then
     MODE=`cat /data/media/0/wipe_mode`
@@ -8,9 +7,10 @@ fi
 if [ ! "disabled" = "$MODE" ]; then
     powercfg $MODE
 fi
-if [[ -f "/system/etc/init.qcom.post_boot.sh" ]] || \
-	[[ -f "/vendor/bin/init.qcom.post_boot.sh" ]] ; then
-	rm -f /dev/.project_wipe
-	rm -f /dev/project_wipe_runonce
-fi
-setenforce $SEstatus
+while read pathtofile
+	do
+    if [ -f "$pathtofile" ]; then
+	    rm -f /dev/.project_wipe
+	    rm -f /dev/project_wipe_runonce
+    fi
+done < ./list_of_magisk
