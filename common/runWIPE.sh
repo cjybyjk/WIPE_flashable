574005af449896be412e488226fe38cae6b5e874
sford <cjybyjk@gmail.com>
2018-07-18 22:05:10 +0800

    script: remove SELinux restore and rewrite some codes

--- common/runWIPE.sh
+++ common/runWIPE.sh
@@ -1,10 +1,4 @@
 # Project WIPE support
-SELinux=`getenforce`
 MODE=`cat /sdcard/wipe_mode`
-if [ "" = "$MODE" ]; then
-    MODE=`cat /data/media/0/wipe_mode`
-fi
-if [ ! "disabled" = "$MODE" ]; then
-    powercfg $MODE > /dev/project_wipe_state
-fi
-setenforce $SELinux
+[ "" = "$MODE" ] && MODE=`cat /data/media/0/wipe_mode`
+[ ! "disabled" = "$MODE" ] && powercfg $MODE > /dev/project_wipe_state
