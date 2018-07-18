53112f7fc881d756c87fff5d6de04380335ab171
sford <cjybyjk@gmail.com>
2018-07-18 22:17:12 +0800

    script: fix no text output

--- common/runWIPE.sh
+++ common/runWIPE.sh
@@ -1,4 +1,5 @@
 # Project WIPE support
 MODE=`cat /sdcard/wipe_mode`
-[ "" = "$MODE" ] && MODE=`cat /data/media/0/wipe_mode`
-[ ! "disabled" = "$MODE" ] && powercfg $MODE > /dev/project_wipe_state
+[ "" == "$MODE" ] && MODE=`cat /data/media/0/wipe_mode`
+[ "disabled" == "$MODE" ] && exit 0
+powercfg $MODE > /dev/project_wipe_state
--- magisk_install/common/service.sh
+++ magisk_install/common/service.sh
@@ -1,8 +1,9 @@
 #!/system/bin/sh
 # Project WIPE support
 MODE=`cat /sdcard/wipe_mode`
-[ "" = "$MODE" ] && MODE=`cat /data/media/0/wipe_mode`
-[ ! "disabled" = "$MODE" ] && powercfg $MODE > /dev/project_wipe_state
+[ "" == "$MODE" ] && MODE=`cat /data/media/0/wipe_mode`
+[ "disabled" == "$MODE" ] && exit 0
+powercfg $MODE > /dev/project_wipe_state
 while read pathtofile
 	do
     if [ -f "$pathtofile" ]; then
