7300395c93ace1a8c9136927cf25a58d094fc8c1
sford <cjybyjk@gmail.com>
2018-07-28 07:22:37 +0800

    fix: magisk install on 16.0+

--- common/runWIPE.sh
+++ common/runWIPE.sh
@@ -3,5 +3,7 @@ SEStatus=`getenforce`
 MODE=`cat /sdcard/wipe_mode`
 [ "" == "$MODE" ] && MODE=`cat /data/media/0/wipe_mode`
 [ "disabled" == "$MODE" ] && exit 0
+rm -f /dev/.project_wipe
+rm -f /dev/project_wipe_runonce
 powercfg $MODE > /dev/project_wipe_state
 setenforce $SEStatus
