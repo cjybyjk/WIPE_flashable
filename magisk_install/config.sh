09e1ae2455fc3eae4fc516ba0d5433977e48dc62
sford <cjybyjk@gmail.com>
2018-08-04 10:19:19 +0800

    test: update for Magisk v16.0+

--- magisk_install/config.sh
+++ magisk_install/config.sh
@@ -1,4 +1,22 @@
 ##########################################################################################
+#
+# Magisk Module Template Config Script
+# by topjohnwu
+#
+##########################################################################################
+##########################################################################################
+#
+# Instructions:
+#
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (config.sh)
+# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
+# 5. Add your additional or modified system properties into common/system.prop
+#
+##########################################################################################
+
+##########################################################################################
 # Configs
 ##########################################################################################
 
@@ -34,9 +52,20 @@ print_modname() {
 # List all directories you want to directly replace in the system
 # Check the documentations for more info about how Magic Mount works, and why you need this
 
+# This is an example
+REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
 # Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
+/system/etc/init.qcom.post_boot.sh
+/vendor/bin/init.qcom.post_boot.sh
+/system/vendor/bin/init.qcom.post_boot.sh
 "
 
 ##########################################################################################
@@ -72,4 +101,3 @@ set_permissions() {
 # update-binary. Refrain from adding code directly into update-binary, as it will make it
 # difficult for you to migrate your modules to newer template versions.
 # Make update-binary as clean as possible, try to only do function calls in it.
-
